03c9729772f2b58a87cda6bcc44dafff7d1b5542
m-makif <77014216+m-makif@users.noreply.github.com>
2021-05-04 18:19:07 +0300

    Create thonny1.py

---
+++ thonny1.py
@@ -0,0 +1,11 @@
+
+##
+
+x = input("Sizce güvenlik zaafiyeti söz konusu olduğunda, temel hak ve özgürlükler devlet tarafından askıya alınmalı mıdır? (Y or N)\n")
+
+if x == "Y": 
+    print("Tebrikler! Aydın bir kişiliksiniz. Günün belli saatlerinde(MASKE-MESAFE-HİJYEN KOŞULUYLA) evden(İŞ VE MARKET ALIŞVERİŞİ İÇİN) çıkmaya hak kazandınız.")
+    
+else:
+    print("Seni gidi komplo teorisyeni, neo-nazi, alt-right, popülist, aşı karşıtı, düz dünyacı etc. piç kurusu!!!!!")
+    
